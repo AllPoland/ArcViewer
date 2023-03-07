6be7597666b2829e668038222ddc6707e10c4527
AllPoland <adff.gaming@gmail.com>
2023-03-07 13:34:19 -0700

    simplify dialogue boxes
    I love layout groups

--- Assets/__Scripts/UI/Popups/DialogueBox.cs
+++ Assets/__Scripts/UI/Popups/DialogueBox.cs
@@ -13,8 +13,6 @@ public class DialogueBox : MonoBehaviour
     [SerializeField] private GameObject cancelButton;
     [SerializeField] private TextMeshProUGUI dialogueText;
 
-    [SerializeField] private float buttonDistance;
-
 
     public void SetText(string text)
     {
@@ -22,60 +20,14 @@ public class DialogueBox : MonoBehaviour
     }
 
 
-    private void SetButtonPosition(GameObject button, float x)
-    {
-        RectTransform buttonTransform = button.GetComponent<RectTransform>();
-        buttonTransform.anchoredPosition = new Vector2(x, buttonTransform.anchoredPosition.y);
-    }
-
-
     public void SetType(DialogueBoxType type)
     {
         boxType = type;
 
-        int buttonCount = 0;
-        switch(type)
-        {
-            case DialogueBoxType.Ok:
-                okButton.SetActive(true);
-                buttonCount = 1;
-                break;
-            case DialogueBoxType.YesNo:
-                yesButton.SetActive(true);
-                noButton.SetActive(true);
-                buttonCount = 2;
-                break;
-            case DialogueBoxType.YesNoCancel:
-                yesButton.SetActive(true);
-                noButton.SetActive(true);
-                cancelButton.SetActive(true);
-                buttonCount = 3;
-                break;
-        }
-
-        float totalButtonDistance = buttonDistance * (buttonCount - 1);
-        float x = -(totalButtonDistance / 2);
-
-        if(okButton.activeInHierarchy)
-        {
-            SetButtonPosition(okButton, x);
-            x += buttonDistance;
-        }
-        if(yesButton.activeInHierarchy)
-        {
-            SetButtonPosition(yesButton, x);
-            x += buttonDistance;
-        }
-        if(noButton.activeInHierarchy)
-        {
-            SetButtonPosition(noButton, x);
-            x += buttonDistance;
-        }
-        if(cancelButton.activeInHierarchy)
-        {
-            SetButtonPosition(cancelButton, x);
-            x += buttonDistance;
-        }
+        okButton.SetActive(type == DialogueBoxType.Ok);
+        yesButton.SetActive(type == DialogueBoxType.YesNo || type == DialogueBoxType.YesNoCancel);
+        noButton.SetActive(type == DialogueBoxType.YesNo || type == DialogueBoxType.YesNoCancel);
+        cancelButton.SetActive(type == DialogueBoxType.YesNoCancel);
     }
 
 
