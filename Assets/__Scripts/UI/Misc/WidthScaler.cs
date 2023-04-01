355ea2b8adf5ab82dfcfb45a423794344894752a
AllPoland <adff.gaming@gmail.com>
2023-03-31 18:14:10 -0600

    add custom width scaling logic
    also moved some scripts around to clear out unnecessary folders

---
+++ Assets/__Scripts/UI/Misc/WidthScaler.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class WidthScaler : MonoBehaviour
+{
+    [SerializeField] private float preferredPadding;
+    [SerializeField] private float minWidth;
+
+    private RectTransform rectTransform;
+    private RectTransform parentTransform;
+
+
+    private void UpdateWidth()
+    {
+        float preferredWidth = parentTransform.rect.width - (preferredPadding * 2);
+
+        Vector2 sizeDelta = rectTransform.sizeDelta;
+        if(preferredWidth >= minWidth)
+        {
+            sizeDelta.x = preferredWidth;
+        }
+        else
+        {
+            sizeDelta.x = Mathf.Min(minWidth, parentTransform.rect.width);
+        }
+        rectTransform.sizeDelta = sizeDelta;
+    }
+
+
+    private void Update()
+    {
+        UpdateWidth();
+    }
+
+
+    private void OnEnable()
+    {
+        if(!rectTransform)
+        {
+            rectTransform = GetComponent<RectTransform>();
+        }
+        if(!parentTransform)
+        {
+            parentTransform = transform.parent.GetComponent<RectTransform>();
+        }
+
+        rectTransform.anchorMin = new Vector2(0.5f, rectTransform.anchorMin.y);
+        rectTransform.anchorMax = new Vector2(0.5f, rectTransform.anchorMax.y);
+    }
+}
