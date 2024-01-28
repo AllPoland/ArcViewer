4b97577a2609558f256e8dc0559bd1802988334e
AllPoland <adff.gaming@gmail.com>
2024-01-27 17:07:10 -0700

    scale down settings tabs with large ui scale
    these were going off-screen and it was funny but not good design

---
+++ Assets/__Scripts/Settings/Elements/TabButtonScaler.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class TabButtonScaler : MonoBehaviour
+{
+    [SerializeField] private float marginSize = 200;
+
+    private Canvas parentCanvas;
+    private RectTransform rectTransform;
+
+
+    private void UpdateScreenSize()
+    {
+        //The tab buttons are sideways, so width is actually vertical on the screen
+        float width = (rectTransform.sizeDelta.x + marginSize) * parentCanvas.scaleFactor;
+        float screenHeight = Screen.height;
+
+        if(width >= screenHeight)
+        {
+            //Scale down to fit in the screen
+            rectTransform.localScale = Vector3.one * (screenHeight / width);
+        }
+        else rectTransform.localScale = Vector3.one;
+    }
+
+
+    private void OnEnable()
+    {
+        if(!parentCanvas)
+        {
+            parentCanvas = GetComponentInParent<Canvas>();
+        }
+        if(!rectTransform)
+        {
+            rectTransform = (RectTransform)transform;
+        }
+
+        ScreenSizeHelper.OnScreenSizeChanged += UpdateScreenSize;
+        UpdateScreenSize();
+    }
+
+
+    private void OnDisable()
+    {
+        ScreenSizeHelper.OnScreenSizeChanged -= UpdateScreenSize;
+    }
+}
