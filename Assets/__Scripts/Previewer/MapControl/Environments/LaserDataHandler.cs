2dcd5e8cfd86061def4fdf78a25aa0c4cd51da29
AllPoland <adff.gaming@gmail.com>
2025-08-01 14:39:07 -0600

    fix diffuse light not updating with settings

--- Assets/__Scripts/Previewer/MapControl/Environments/LaserDataHandler.cs
+++ Assets/__Scripts/Previewer/MapControl/Environments/LaserDataHandler.cs
@@ -20,7 +20,7 @@ public class LaserDataHandler : MonoBehaviour
     private Vector4[] occlusionFalloffInfos;
 
 
-    private void UpdateLaserBuffer()
+    private void UpdateLaserBuffer(bool forceUpdate = false)
     {
         float brightnessMult = SettingsManager.Loaded ? SettingsManager.GetFloat("lightglowbrightness") : 1f;
 
@@ -62,7 +62,7 @@ public class LaserDataHandler : MonoBehaviour
                 continue;
             }
 
-            if(!lightHandler.Dirty)
+            if(!lightHandler.Dirty && !forceUpdate)
             {
                 continue;
             }
@@ -97,7 +97,7 @@ public class LaserDataHandler : MonoBehaviour
     }
 
 
-    private void UpdateOcclusionBuffer()
+    private void UpdateOcclusionBuffer(bool forceUpdate = false)
     {
         if(occlusionColors == null || occlusionColors.Length != MaxOcclusions)
         {
@@ -136,7 +136,7 @@ public class LaserDataHandler : MonoBehaviour
                 continue;
             }
 
-            if(!occlusionMarker.Dirty)
+            if(!occlusionMarker.Dirty && !forceUpdate)
             {
                 continue;
             }
@@ -170,6 +170,16 @@ public class LaserDataHandler : MonoBehaviour
     }
 
 
+    private void UpdateSettings(string setting)
+    {
+        if(setting == "all" || setting == "lightglowbrightness")
+        {
+            //Force update the laser buffer to account for change in brightness settings
+            UpdateLaserBuffer(true);
+        }
+    }
+
+
     private void UpdateBuffers()
     {
         UpdateLaserBuffer();
@@ -185,6 +195,13 @@ public class LaserDataHandler : MonoBehaviour
 
     private void OnEnable()
     {
+        SettingsManager.OnSettingsUpdated += UpdateSettings;
         UpdateBuffers();
+    }
+
+
+    private void OnDisable()
+    {
+        SettingsManager.OnSettingsUpdated -= UpdateSettings;
     }
 }
