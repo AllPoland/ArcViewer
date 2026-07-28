402b8478c00a0edcb1729e2e395a9f71ff240f58
AllPoland <52187288+AllPoland@users.noreply.github.com>
2026-07-27 21:37:50 -0600

    move FPV button to be with its friends
    it was lonely over at the player

--- Assets/__Scripts/UI/Previewer/PovButton.cs
+++ Assets/__Scripts/UI/Previewer/PovButton.cs
@@ -5,6 +5,12 @@ public class PovButton : MonoBehaviour
     [SerializeField] private QuickSettingButton button;
 
 
+    private void OnEnable()
+    {
+        button.gameObject.SetActive(ReplayManager.IsReplayMode);
+    }
+
+
     private void Update()
     {
         if(ReplayManager.IsReplayMode && !EventSystemHelper.SelectedObject && Input.GetButtonDown("TogglePerspective"))
